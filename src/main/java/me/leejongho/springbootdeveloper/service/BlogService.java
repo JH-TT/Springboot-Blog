502737e6601e9e4ef0b3f0e6bfc6f615a8fcbd03
JH-TT <leejongho9803@gmail.com>
2023-06-17 17:54:04 +0900

    feat(oauth) - OAuth 뷰 구성하기

--- src/main/java/me/leejongho/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/leejongho/springbootdeveloper/service/BlogService.java
@@ -5,6 +5,7 @@ import me.leejongho.springbootdeveloper.domain.Article;
 import me.leejongho.springbootdeveloper.dto.AddArticleRequest;
 import me.leejongho.springbootdeveloper.dto.UpdateArticleRequest;
 import me.leejongho.springbootdeveloper.repsitory.BlogRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,7 +31,10 @@ public class BlogService {
     }
 
     public void delete(long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found : " + id));
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional
@@ -38,8 +42,17 @@ public class BlogService {
         Article article = blogRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("not found: " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    // 게시글을 작성한 유저인지 확인
+    private static void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
