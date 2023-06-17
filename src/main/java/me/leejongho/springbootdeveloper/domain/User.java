713f51c0b82d0f32dd34a6bde70f72dc44727164
JH-TT <leejongho9803@gmail.com>
2023-06-17 16:37:58 +0900

    feat(oauth2) - OAuth2 서비스 구현하기

--- src/main/java/me/leejongho/springbootdeveloper/domain/User.java
+++ src/main/java/me/leejongho/springbootdeveloper/domain/User.java
@@ -29,10 +29,14 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String auth, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
     @Override // 권한 반환
@@ -72,5 +76,12 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         // 계정 사용 가능 여부 반환
         return true;
+    }
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
     }
 }
